3744befadcaa10b34aee0f295ebbcc02e33a2b6d
vanhuong dong <vanhuongv22@gmail.com>
2022-10-18 10:54:19 +0700

    shop liss invitation

--- src/common/utils/axios.ts
+++ src/common/utils/axios.ts
@@ -23,10 +23,10 @@ axiosInstance.interceptors.request.use(async (config) => {
     try {
       const authLogin = JSON.parse(getPersist).authLogin;
 
-      // const token = JSON.parse(authLogin).accessToken;
+      const token = JSON.parse(authLogin).accessToken;
 
-      const token =
-        'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiYW9sYW0wMzA3QGdtYWlsLmNvbSIsIlVzZXJUeXBlIjoiQURNSU4iLCJpYXQiOjE2NjYwMTQxNTAsImV4cCI6MjM4NjAxNDE1MH0.pA26R7T8ICmiVTDBDGLdyVwdpe_U93v-BzarArhQEypOYgvHfBotWXycGcv4sX6OBVGN_SNuDfQQHK0YZWcozA';
+      // const token =
+      //   'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiYW9sYW0wMzA3QGdtYWlsLmNvbSIsIlVzZXJUeXBlIjoiQURNSU4iLCJpYXQiOjE2NjYwMTQxNTAsImV4cCI6MjM4NjAxNDE1MH0.pA26R7T8ICmiVTDBDGLdyVwdpe_U93v-BzarArhQEypOYgvHfBotWXycGcv4sX6OBVGN_SNuDfQQHK0YZWcozA';
       config.headers = {
         ...config.headers,
         Authorization: `Bearer ${token}`,
