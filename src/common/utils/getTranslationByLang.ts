6b71eb0c1cf931c16fc17d5455f078615ed1731d
ducpt-bili <duc.bilisoftware@gmail.com>
2022-10-20 16:30:13 +0700

    Fix the deployment 5

--- src/common/utils/getTranslationByLang.ts
+++ src/common/utils/getTranslationByLang.ts
@@ -3,7 +3,7 @@ import { LANG } from 'src/common/constants/common.interfaces';
 export function getTranslationByLang<Type>(lang: LANG, translates: Array<Type>): Type {
   let translation: Type = {} as Type;
   translates.forEach((translate) => {
-    for (const [key, value] of Object.entries(translate as object)) {
+    for (const [key, value] of Object.entries(translate as any)) {
       if (key === 'lang' && value === lang) {
         translation = { ...translate };
       }
