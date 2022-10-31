c39c458df6321b16cb84fbf1e0d22a21fb4d614e
Long Nguyen <longnntptit@gmail.com>
2022-10-31 14:47:14 +0700

    fix #776

--- src/event-promotion-IV/ListEvent/index.tsx
+++ src/event-promotion-IV/ListEvent/index.tsx
@@ -7,12 +7,38 @@ import { EventTable } from './EventTable';
 
 import { useNavigate } from 'react-router-dom';
 import { EventTableToolbar } from './EventTableToolbar';
+import {
+  selectedIdsState,
+  setIsResetSelect,
+  setSelectedIds,
+} from '../eventPromotionIV.slice';
+import { dispatch, useSelector } from 'src/common/redux/store';
+import { useDeleteEvents } from '../hooks/useDeleteEvent';
+import useMessage from 'src/store-admin/hooks/useMessage';
 
 export default function ListEventPromotionDashboard() {
   const navigate = useNavigate();
 
+  const selectedIdsValue = useSelector(selectedIdsState);
+
   const handleCreateEvent = () => {
     navigate(PATH_DASHBOARD.eventPromotionIV.new);
+  };
+
+  const { showSuccessSnackbar, showErrorSnackbar } = useMessage();
+
+  const mutationDelete = useDeleteEvents({
+    onSuccess: () => showSuccessSnackbar('Xóa sự kiện thành công'),
+    onError: () => showErrorSnackbar('Xóa sự kiện thất bại'),
+    onSuccessSend: () => showErrorSnackbar('Sự kiện đã có người trúng không thể xóa'),
+  });
+
+  const handleDeleteRows = () => {
+    if (selectedIdsValue.length) {
+      mutationDelete.mutate(selectedIdsValue);
+      dispatch(setSelectedIds([]));
+      dispatch(setIsResetSelect(true));
+    }
   };
 
   return (
@@ -24,13 +50,24 @@ export default function ListEventPromotionDashboard() {
           { name: 'Danh sách sự kiện' },
         ]}
         action={
-          <Button
-            variant="contained"
-            startIcon={<Iconify icon={'akar-icons:file'} />}
-            onClick={handleCreateEvent}
-          >
-            Tạo mới
-          </Button>
+          <>
+            <Button
+              variant="contained"
+              startIcon={<Iconify icon={'akar-icons:file'} />}
+              onClick={handleCreateEvent}
+              sx={{ mr: '10px' }}
+            >
+              Tạo mới
+            </Button>
+            <Button
+              variant="contained"
+              startIcon={<Iconify icon={'akar-icons:file'} />}
+              color="error"
+              onClick={handleDeleteRows}
+            >
+              Xóa
+            </Button>
+          </>
         }
       />
       <Card sx={{ p: '10px', w: '100%' }}>
