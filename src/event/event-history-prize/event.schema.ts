070fa4afb9db722aa1565a10f890633d99b2d9e5
Bui Huu Thinh <bhtkhtn@gmail.com>
2022-11-15 12:46:24 +0700

    format-split-component-form

--- src/event/event-history-prize/event.schema.ts
+++ src/event/event-history-prize/event.schema.ts
@@ -17,10 +17,6 @@ export const createEventPrizevalidate = () => {
 
   const createEventPrizeSchema = Yup.object().shape({
     giftId: Yup.number().required('This field is required').typeError('Must be a number'),
-    popupCode: Yup.string()
-      .typeError('Must be a string')
-      .required('This field is required')
-      .test('test empty', 'Content is required', (val) => val !== ''),
     popUpCodeTitle: Yup.string()
       .max(60, 'Title must be less than 60 characters')
       .min(25, 'Title must be greater than 25 characters')
@@ -45,9 +41,6 @@ export const createEventPrizevalidate = () => {
     }),
     giftStatus: Yup.boolean(),
     popupImageLink: Yup.string()
-      .required('This field is required')
-      .typeError('Must be a string'),
-    popupType: Yup.string()
       .required('This field is required')
       .typeError('Must be a string'),
     notificationContent: Yup.string()
