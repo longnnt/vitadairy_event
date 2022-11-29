8a4f3a9cc96b0b57419da407ec58b853ebaaa590
Bui Huu Thinh <bhtkhtn@gmail.com>
2022-11-29 17:40:57 +0700

    fix-comment

--- src/common/components/hook-form/RHFSelect.tsx
+++ src/common/components/hook-form/RHFSelect.tsx
@@ -14,25 +14,24 @@ type Props = IProps & TextFieldProps;
 
 export default function RHFSelect({ name, children, ...other }: Props) {
   const { control } = useFormContext();
+
   return (
     <Controller
       name={name}
       control={control}
-      render={({ field, fieldState: { error } }) => {
-        return (
-          <TextField
-            {...field}
-            select
-            fullWidth
-            SelectProps={{ native: true }}
-            error={!!error}
-            helperText={error?.message}
-            {...other}
-          >
-            {children}
-          </TextField>
-        )
-      } }
+      render={({ field, fieldState: { error } }) => (
+        <TextField
+          {...field}
+          select
+          fullWidth
+          SelectProps={{ native: true }}
+          error={!!error}
+          helperText={error?.message}
+          {...other}
+        >
+          {children}
+        </TextField>
+      )}
     />
   );
 }
