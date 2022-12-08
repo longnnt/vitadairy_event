9d254f47202c80ec98a20d674d703289d4758d45
Bui Huu Thinh <bhtkhtn@gmail.com>
2022-12-08 09:46:59 +0700

    set-validate-phone-number

--- src/store-admin/storeAdmin.schema.ts
+++ src/store-admin/storeAdmin.schema.ts
@@ -4,5 +4,5 @@ export const EditStoreAdminSchema = Yup.object().shape({
   name: Yup.string().required('This field is required'),
   code: Yup.string().required('This field is required'),
   address: Yup.string().required('This field is required'),
-  phoneNumber: Yup.string().required('This field is required'),
+  phoneNumber: Yup.string().min(10, 'Phone number must be greater than 10 characters'),
 });
