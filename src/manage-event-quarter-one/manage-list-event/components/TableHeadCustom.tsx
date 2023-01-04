badc190097a64c3c500d03185be6a9585f8e3421
Bui Huu Thinh <bhtkhtn@gmail.com>
2023-01-04 14:17:06 +0700

    fix-comment

--- src/manage-event-quarter-one/manage-list-event/components/TableHeadCustom.tsx
+++ src/manage-event-quarter-one/manage-list-event/components/TableHeadCustom.tsx
@@ -76,25 +76,7 @@ export default function TableHeadCustom({
             sortDirection={orderBy === headCell.id ? order : false}
             sx={{ width: headCell.width, minWidth: headCell.minWidth }}
           >
-            {/* {onSort ? (
-              <TableSortLabel
-                hideSortIcon
-                active={orderBy === headCell.id}
-                direction={orderBy === headCell.id ? order : 'asc'}
-                onClick={() => onSort(headCell.id)}
-                sx={{ textTransform: 'capitalize' }}
-              >
-                {headCell.label}
-
-                {orderBy === headCell.id ? (
-                  <Box sx={{ ...visuallyHidden }}>
-                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                  </Box>
-                ) : null}
-              </TableSortLabel>
-            ) : ( */}
-              {headCell.label}
-            {/* )} */}
+            {headCell.label}
           </TableCell>
         ))}
       </TableRow>
