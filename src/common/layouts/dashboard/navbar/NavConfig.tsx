67d633392333db5b43665384db719c49dbfe451e
Bui Huu Thinh <bhtkhtn@gmail.com>
2023-01-04 15:37:56 +0700

    save-work

--- src/common/layouts/dashboard/navbar/NavConfig.tsx
+++ src/common/layouts/dashboard/navbar/NavConfig.tsx
@@ -97,10 +97,6 @@ const navConfig = [
             path: PATH_DASHBOARD.manageEventQuarterOne.list,
           },
           {
-            title: BREADCUMBS.MANAGE_CREATE_EVENT,
-            path: PATH_DASHBOARD.manageEventQuarterOne.new,
-          },
-          {
             title: BREADCUMBS.EVENT_PRIZE,
             path: PATH_DASHBOARD.eventPrizeQ1.list
           }
