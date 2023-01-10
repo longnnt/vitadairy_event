06a31f413149ec134c968420095b34f7d91e6e53
Cuong Dang <dangcuong1225@gmail.com>
2023-01-10 10:00:24 +0700

    fix axios instance

--- src/manage-event-quarter-one/services.ts
+++ src/manage-event-quarter-one/services.ts
@@ -1,27 +1,38 @@
-import { API_EVENT_GROUP, API_MANAGE_EVENT, API_PRODUCT } from './../common/constants/apis';
-import axiosInstanceV2 from 'src/common/utils/axiosV2';
-import { IEventGroupParams, IEventSearchParams, IDataListEvent,IPostCreateEvent, IManageEventParams, IDataListEventGroup, IStatusParams } from './common/interface';
+import {
+  API_EVENT_GROUP,
+  API_MANAGE_EVENT,
+  API_PRODUCT,
+} from './../common/constants/apis';
+import {
+  IEventGroupParams,
+  IEventSearchParams,
+  IDataListEvent,
+  IPostCreateEvent,
+  IManageEventParams,
+  IDataListEventGroup,
+  IStatusParams,
+} from './common/interface';
 import axiosInstance from 'src/common/utils/axios';
 
 export const getListEventAdmin = (params: IManageEventParams) => {
-  if(params.searchBy === '') {
-    delete params['searchBy']
+  if (params.searchBy === '') {
+    delete params['searchBy'];
   }
-  if(params.searchText === '') {
-    delete params['searchText']
+  if (params.searchText === '') {
+    delete params['searchText'];
   }
-  if(params.status === '') {
-    delete params['status']
+  if (params.status === '') {
+    delete params['status'];
   }
-  return axiosInstanceV2.get<unknown, IDataListEvent>(`${API_MANAGE_EVENT}`, {params});
+  return axiosInstance.get<unknown, IDataListEvent>(`${API_MANAGE_EVENT}`, { params });
 };
 
 export const postCreateEventAdmin = (data: IPostCreateEvent) => {
-  return axiosInstanceV2.post(`${API_MANAGE_EVENT}`, data);
-}
+  return axiosInstance.post(`${API_MANAGE_EVENT}`, data);
+};
 
 export const deleteEventId = (id: number) => {
-  return axiosInstanceV2.delete(`${API_MANAGE_EVENT}/${id}`);
+  return axiosInstance.delete(`${API_MANAGE_EVENT}/${id}`);
 };
 
 export const getProductCode = (params: IEventSearchParams) => {
@@ -29,9 +40,9 @@ export const getProductCode = (params: IEventSearchParams) => {
 };
 
 export const getEventGroup = () => {
-  return axiosInstanceV2.get<unknown, IDataListEventGroup>(`${API_EVENT_GROUP}`);
+  return axiosInstance.get<unknown, IDataListEventGroup>(`${API_EVENT_GROUP}`);
 };
 
 export const patchEventStatus = (data: IStatusParams) => {
-  return axiosInstanceV2.patch(API_MANAGE_EVENT, data)
-}
+  return axiosInstance.patch(API_MANAGE_EVENT, data);
+};
