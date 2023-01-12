90b343d905cab0ffbcd12d4fcb2d19dae41e7b9e
RimiruSama <bavuong22897@gmail.com>
2023-01-12 17:03:45 +0700

    edit table list prize

--- src/event-prize-q1/hooks/useGetListPrize.ts
+++ src/event-prize-q1/hooks/useGetListPrize.ts
@@ -1,12 +1,13 @@
+import { IListPrizeParams } from './../../event/list-prize/interfaces';
 import { QUERY_KEYS } from 'src/common/constants/queryKeys.constant';
 import { useQuery } from "react-query"
 import { getListPrize } from '../services';
 
-export const useGetListPrize = (eventId: number) => {
+export const useGetListPrize = (params: IListPrizeParams) => {
     return {
         ...useQuery(
-            [QUERY_KEYS.EVENT_LIST_PRIZE_Q1, { eventId }],
-            () => getListPrize(eventId), {cacheTime: 0}
+            [QUERY_KEYS.EVENT_LIST_PRIZE_Q1, params],
+            () => getListPrize(params), {cacheTime: 0}
         )
     }
 }
