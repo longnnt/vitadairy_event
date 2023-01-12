a97d8ef252b65857d27db83007e10f220c5922fe
Bui Huu Thinh <bhtkhtn@gmail.com>
2023-01-12 14:50:54 +0700

    fix-bug-cannot-show-format-date

--- src/event-history-prize/list-event-history-prize/components/HistoryTable.tsx
+++ src/event-history-prize/list-event-history-prize/components/HistoryTable.tsx
@@ -1,15 +1,22 @@
 import { TableCell, TableRow } from '@mui/material';
 
 import dayjs from 'dayjs';
-import {
-  FORMAT_DATE_FILTER
-} from 'src/common/constants/common.constants';
+import _ from 'lodash';
+import { FORMAT_DATE_FILTER, FORMAT_TEST } from 'src/common/constants/common.constants';
 import { IPropsPrizeHistoryTableRow } from '../../common/interface';
 
 // ----------------------------------------------------------------------
 
 function PrizeHistoryTableRow({ row, selected }: IPropsPrizeHistoryTableRow) {
   const { id, phoneNumber, userName, qr, giftName, giftReceivedDate, spoonCode } = row;
+  const time = giftReceivedDate.split(' ')[0].replace(',', '');
+  const date = giftReceivedDate.split(' ')[1].split('/');
+  const day = date[0];
+  let month = date[1];
+  if (parseInt(month) < 10) {
+    month = '0' + month;
+  }
+  const year = date[2];
   return (
     <TableRow hover selected={selected}>
       <TableCell align="left">{id}</TableCell>
@@ -20,7 +27,7 @@ function PrizeHistoryTableRow({ row, selected }: IPropsPrizeHistoryTableRow) {
       <TableCell align="left" sx={{ textTransform: 'capitalize' }}>
         {dayjs(giftReceivedDate).isValid()
           ? dayjs(giftReceivedDate).format(FORMAT_DATE_FILTER)
-          : ''}
+          : `${month}-${day}-${year} ${time}`}
       </TableCell>
 
       <TableCell align="left">{qr}</TableCell>
