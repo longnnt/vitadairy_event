5e5a28e48aa170d778b169b8430107e929366b2b
Bui Huu Thinh <bhtkhtn@gmail.com>
2023-01-12 16:16:12 +0700

    fix-comment

---
+++ src/event-history-prize/common/utils/formatDate.ts
@@ -0,0 +1,14 @@
+import { IPrizeHistory } from '../interface';
+
+export function formatDates(row: IPrizeHistory) {
+  const { giftReceivedDate } = row;
+  const time = giftReceivedDate.split(' ')[0].replace(',', '');
+  const date = giftReceivedDate.split(' ')[1].split('/');
+  const day = date[0];
+  let month = date[1];
+  if (parseInt(month) < 10) {
+    month = '0' + month;
+  }
+  const year = date[2];
+  return `${month}-${day}-${year} ${time}`
+}
--- src/event-history-prize/list-event-history-prize/components/HistoryTable.tsx
+++ src/event-history-prize/list-event-history-prize/components/HistoryTable.tsx
@@ -2,20 +2,13 @@ import { TableCell, TableRow } from '@mui/material';
 
 import dayjs from 'dayjs';
 import { FORMAT_DATE_FILTER } from 'src/common/constants/common.constants';
+import { formatDates } from 'src/event-history-prize/common/utils/formatDate';
 import { IPropsPrizeHistoryTableRow } from '../../common/interface';
 
 // ----------------------------------------------------------------------
 
 function PrizeHistoryTableRow({ row, selected }: IPropsPrizeHistoryTableRow) {
   const { id, phoneNumber, userName, qr, giftName, giftReceivedDate, spoonCode } = row;
-  const time = giftReceivedDate.split(' ')[0].replace(',', '');
-  const date = giftReceivedDate.split(' ')[1].split('/');
-  const day = date[0];
-  let month = date[1];
-  if (parseInt(month) < 10) {
-    month = '0' + month;
-  }
-  const year = date[2];
   return (
     <TableRow hover selected={selected}>
       <TableCell align="left">{id}</TableCell>
@@ -26,7 +19,7 @@ function PrizeHistoryTableRow({ row, selected }: IPropsPrizeHistoryTableRow) {
       <TableCell align="left" sx={{ textTransform: 'capitalize' }}>
         {dayjs(giftReceivedDate).isValid()
           ? dayjs(giftReceivedDate).format(FORMAT_DATE_FILTER)
-          : `${month}-${day}-${year} ${time}`}
+          : formatDates(row)}
       </TableCell>
 
       <TableCell align="left">{qr}</TableCell>
